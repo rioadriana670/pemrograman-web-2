5909a44b459e4af4b3d4c59cf079cad69e7c7466
RioA <rioadriana670@gmail.com>
2026-04-11 11:46:53 +0700

    Add files via upload

---
+++ pertemuan 5/counter.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Contoh Counter</title>
+</head>
+<body>
+ <?php
+ $nama_file="counter.dat";
+ If (file_exists($nama_file))
+ {
+  $berkas = fopen($nama_file,"r");
+  $pencacah = (integer)trim(fgets($berkas, 255));
+  $pencacah++;
+  Fclose($berk
+}
+ Else
+  $pencacah = 1;
+  // simpan pencacah
+  $berkas = fopen($nama_file,"w");
+  Fputs($berkas, $pencacah);
+  Fclose($berkas);
+ 
+  // tulis ke halaman web
+  Print("Anda pengunjung ke-$pencacah <br>\n");   ?>
+</body>
+</html>
